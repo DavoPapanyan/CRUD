70ca09c919cef543bd1df0e56605c2a28a76beae
DavMInass <davitminasyan95@gmail.com>
2024-01-26 21:32:17 +0400

    info

--- server.js
+++ server.js
@@ -3,44 +3,52 @@ var path = require("path");
 const bodyParser = require("body-parser");
 var app = express();
 const mongoose = require('mongoose');
+const { log } = require("console");
 const connectionString = "mongodb+srv://davitpapanyan1:David28022008@cluster0.ekkgmwy.mongodb.net/sample_mflix";
 
-app.use(bodyParser.urlencoded({extended: true}));
+
+app.set("view engine", "ejs");
+
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 app.use(express.static("public"));
 
-app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "./public/form.html"));
+app.get("/", function (req, res) {
+    const info = [
+        // { name: 'Sammy', organization: "DigitalOcean", birth_year: 2012},
+        // { name: 'Tux', organization: "Linux", birth_year: 1996},
+        // { name: 'Moby Dock', organization: "Docker", birth_year: 2013}
+    ];
+    res.render('../public/form.ejs', {
+        obj: info
+    });
 });
 
 
 
-app.listen(3000, function() {
+app.listen(3000, function () {
     console.log("Example is running on port 3000");
 });
 
 
 app.post('/addName', async (req, res) => {
-  const name = req.body.name;
-  const password = req.body.password;
-  const email = req.body.email;
-  mongoose.connect(connectionString, { useNewUrlParser: true, useUnifiedTopology: true });
-  const db = mongoose.connection;
-  db.on('error', console.error.bind(console, 'Connection error:'));
-  db.once('open', async () => {
-      console.log('Connected to MongoDB!');
-      try {
-         let result = await mongoose.connection.db.collection('users').insertOne({
-              name: name,
-              email: email,
-              password: password
-          })
-          res.json(result);
-      } catch (error) {
-          console.error('Error retrieving movies:', error);
-      } finally {
-          mongoose.connection.close();
-      }
-  })
-});
+    //   const name = req.body.name;
+    //   const password = req.body.password;
+    //   const email = req.body.email;
+    mongoose.connect(connectionString, { useNewUrlParser: true, useUnifiedTopology: true });
+    const db = mongoose.connection;
+    db.on('error', console.error.bind(console, 'Connection error:'));
+    db.once('open', async () => {
+        try {
+            let result = await mongoose.connection.db.collection('theaters').find({ 'location.address.city': 'Bloomington' }).toArray()
+            res.render('../public/form.ejs', {
+                obj: result
+            });
+        } catch (error) {
+            console.error('Error retrieving movies:', error);
+        } finally {
+            mongoose.connection.close();
+        }
+    })
+})
